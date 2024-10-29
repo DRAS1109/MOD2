e66952577fc51387d0c565c1c72ab189d57c0de6
DRAS1109 <25049@esenviseu.net>
2024-10-29 09:51:48 +0000

    MOD2

---
+++ 37.py
@@ -0,0 +1,28 @@
+#Este programa calcula os pontos restantes da carta de condução
+Muito_grave = 0
+Grave = 0
+Leve = 0
+Infração = 0
+
+Pontos = int(input("Introduza o numero de pontos disponiveis: "))
+
+while Infração != 4:
+    print(f"Tem {Pontos} pontos restantes.")
+    print("Escoha uma opção (O numero correspondente à  infração):")
+    Infração = int(input("1- Muito grave, 2- Grave, 3- Leve, 4- Terminar: "))
+
+    if Infração == 1:
+        Muito_grave = Muito_grave + 1
+        Pontos = Pontos - 6
+
+    elif Infração == 2:
+        Grave = Grave + 1
+        Pontos = Pontos - 4
+
+    elif Infração == 3:
+        Leve = Leve + 1
+        if Leve != 1:
+            Pontos = Pontos - 1
+
+    if Muito_grave == 1 and Grave == 1 or Grave == 2 or Pontos <= 0:
+        print("Perdeu a sua carta")
