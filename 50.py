2eda060b6291d22e193961a4dc1d15e5af412037
DRAS1109 <25049@esenviseu.net>
2024-11-12 08:29:34 +0000

    12/11/2024

--- 50.py
+++ 50.py
@@ -1,37 +1,7 @@
-#Criar palavra passe segura
+#Jogo da forca !!!
 """
-Tem de ter pelo menos um:
--letra maiuscula e minuscula
--numero
--8 caracteres
--caracteres especiais
+ o
+/|\
+ |
+/ \
 """
-import random
-Palavra_Passe = ""
-
-Numeros = ("1234567890")
-Caracteres_Especiais = ("!#$%&/()=?@£§€{[]}'«»´`~^ºª-_.:,;<>\\|")
-alfabeto = ("abcdefghijklmnopqrstuvwxyz")
-
-N_Caracteres = random.randint (8, 20)
-
-for _ in range (0, N_Caracteres):
-    Ação = random.randint (1, 4)
-
-    if Ação == 1:
-        N = random.randint (0, 9)
-        Palavra_Passe = Palavra_Passe + str(N)
-
-    if Ação == 2:
-        L = random.randint (0, len(alfabeto.upper()))
-        Palavra_Passe = Palavra_Passe + alfabeto.upper()[L]
-
-    if Ação == 3:
-        l = random.randint (0, len(alfabeto))
-        Palavra_Passe = Palavra_Passe + alfabeto[l]
-    
-    if Ação == 4:
-        C = random.randint (0, len(Caracteres_Especiais))
-        Palavra_Passe = Palavra_Passe + Caracteres_Especiais[C]
-
-print (f"Uma dica de palavra passe é: {Palavra_Passe}")
