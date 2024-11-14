4f12165bee15db224c758f5058871a55a748eb27
DRAS1109 <25049@esenviseu.net>
2024-11-14 13:08:08 +0000

    14/11/2024

--- 50.py
+++ 50.py
@@ -1,7 +1,30 @@
-#Jogo da forca !!!
 """
- o
-/|\
- |
-/ \
+Este programa vai gerir o embarque num aviao
+Nº de lugares do aviao
+Nº de bilhetes totais
+Ler o nome dos passageiros e guardar nua string separados por \n
+Se inserir um nome em branco o programa termina e mostra os nomes dos clientes e quantos entraram
+O mesmo se o aviao estiver cheio
 """
+
+N_Passageiros = 0
+Todos_Nomes = ""
+
+N_Lugares = int(input("Introduza o Nº de lugares do avião: "))
+N_Bilhetes = int(input("Introduza o Nº de bilhetes vendidos: "))
+
+for i in range (1, N_Bilhetes + 1):
+    if N_Bilhetes > N_Lugares:
+        print ("Overbooking é proibido, ligando para as autoridades para alertar... \n")
+        break
+    Nome = input(f"Introduza o nome do {i}º cliente: ")
+    
+    if Nome != "":
+        N_Passageiros = N_Passageiros + 1
+        Todos_Nomes = Todos_Nomes + Nome + " \n"
+
+    else:
+        print (f"Entraram {N_Passageiros} passageiros e os nomes são:")
+        break
+
+print (Todos_Nomes)
---
+++ 51.py
@@ -0,0 +1,9 @@
+"""
+Um produto de um supermercado está em promoção.
+As primeiras 10 unidades custam 10€ cada
+Se comprar mais de 10 tem direito a um desconto de 5%, este desconto só aplicado ao número de unidades para além das 10 iniciais
+Se comprar mais de 20 tem direito a um desconto de 10% nas unidades que excedem as 20 iniciais
+Faça um programa que pergunte quantas unidades o cliente comprou e calcule o preço total a pagar
+"""
+
+Para terminar
